044ceae4c8b99d58d6fd7417bf5415946c435b1b
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2022-04-10 13:07:16 +0200

    allow for reading from config file database names

--- baza/cli.py
+++ baza/cli.py
@@ -1,12 +1,27 @@
 import subprocess
+import configparser
 
 import typer
 
 
-def main():
-    process = subprocess.run(["mongo"])
-    print(f"Mongo exited ({process.returncode})")
+DEFAULT_CONFIG_FILES_NAMES = [
+    '.bazarc',
+]
 
+def main(selector: str):
+    config = configparser.ConfigParser()
+    config.read(DEFAULT_CONFIG_FILES_NAMES)
+
+    if selector not in config.sections():
+        typer.echo(f'{selector} is not a valid selector')
+        return
+    
+    connection_string = config.get(selector, 'connection_string')
+    if connection_string is None:
+        typer.echo(f'{selector} has no connection string')
+        return
+
+    process = subprocess.run(["mongosh", connection_string])
 
 def cli():
     typer.run(main)
